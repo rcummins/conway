62eac7792087aa3cb911cbb5109a84bb19fbde5b
Renata Cummins <renatacummins@gmail.com>
2020-05-26 08:33:51 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const canvasElement = document.getElementById('game-canvas');
+  canvasElement.width = window.innerWidth;
+  canvasElement.height = window.innerHeight;
+
+  const ctx = canvasElement.getContext('2d');
+});
