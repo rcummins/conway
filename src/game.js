67a08551e48d2cfec8d4aa7f9c8f411dd813ffb8
Renata Cummins <renatacummins@gmail.com>
2020-05-29 13:30:43 -0400

    feature: add function to advance game by 1 step, with tests

--- src/game.js
+++ src/game.js
@@ -29,10 +29,51 @@ class Game {
   }
 
   step() {
-    for (let row = 0; row < this.numRows; row++) {
-      for (let col = 0; col < this.numCols; col++) {
-        this.grid[row][col].isAlive = !this.grid[row][col].isAlive;
+    // make an array of placeholder objects whose isAlive property matches that
+    // of the cells in the first row of the grid, to store the current state
+    // of the first row to use when calculating the next state of the last row
+    const firstRowBuffer = [];
+    for (let colIndex = 0; colIndex < this.numCols; colIndex++) {
+      firstRowBuffer.push({ isAlive: this.grid[0][colIndex].isAlive });
+    }
+
+    // set up the variables needed to loop through all the rows
+    const lastRowIndex = this.numRows - 1;
+    let currRowIsAlive = null;
+    let prevRowIsAlive = this.rowAliveNextStep(
+      this.grid[lastRowIndex],
+      this.grid[0],
+      this.grid[1]);
+
+    // loop through all rows except the first and last row
+    for (let rowIndex = 1; rowIndex < lastRowIndex; rowIndex++) {
+      // calculate isAlive property in the next step for cells in current row
+      currRowIsAlive = this.rowAliveNextStep(
+        this.grid[rowIndex - 1],
+        this.grid[rowIndex],
+        this.grid[rowIndex + 1]
+      );
+
+      // update the isAlive property for cells in the previous row
+      for (let colIndex = 0; colIndex < this.numCols; colIndex++) {
+        this.grid[rowIndex - 1][colIndex].isAlive = prevRowIsAlive[colIndex];
       }
+
+      // save current row as prevRow so it can be updated in next interation
+      prevRowIsAlive = currRowIsAlive;
+    }
+
+    // calculate isAlive property in the next step for cells in the last row
+    currRowIsAlive = this.rowAliveNextStep(
+      this.grid[lastRowIndex - 1],
+      this.grid[lastRowIndex],
+      firstRowBuffer
+    );
+
+    // update the isAlive property for cells in the last two rows
+    for (let colIndex = 0; colIndex < this.numCols; colIndex++) {
+      this.grid[lastRowIndex - 1][colIndex].isAlive = prevRowIsAlive[colIndex];
+      this.grid[lastRowIndex][colIndex].isAlive = currRowIsAlive[colIndex];
     }
   }
 
--- src/game.test.js
+++ src/game.test.js
@@ -1,5 +1,5 @@
 const Game = require('./game');
-const game = new Game(100, 100);
+const game = new Game(1000, 1000);
 
 // initialize placeholder objects for dead and alive cells
 const deadCell = { isAlive: false };
@@ -91,5 +91,76 @@ describe('rowAliveNextStep', () => {
     expect(result3[2]).toBe(false);
     expect(result3[3]).toBe(true);
     expect(result3[4]).toBe(false);
+  });
+});
+
+describe('step', () => {
+  // first change all cells isAlive to false to overwrite random assignments
+  // const numRows = game.grid.length;
+  // const numCols = game.grid[0].length;
+  for (let row = 0; row < game.numRows; row++) {
+    for (let col = 0; col < game.numCols; col++) {
+      game.grid[row][col].isAlive = false;
+    }
+  }
+
+  // set up blinker in the middle of the grid
+  game.grid[25][24].isAlive = true;
+  game.grid[25][25].isAlive = true;
+  game.grid[25][26].isAlive = true;
+
+  // set up blinker at the top of the grid (wrapping to the bottom)
+  game.grid[0][14].isAlive = true;
+  game.grid[0][15].isAlive = true;
+  game.grid[0][16].isAlive = true;
+
+  // set up blinker in the corner of the grid (wrapping to all other corners)
+  game.grid[0][49].isAlive = true;
+  game.grid[0][0].isAlive = true;
+  game.grid[0][1].isAlive = true;
+
+  // advance the game one step
+  game.step();
+
+  test('isAlive correct after 1 step for non-edge cells', () => {
+    expect(game.numRows).toBe(50);
+    expect(game.numCols).toBe(50);
+    expect(game.grid[24][24].isAlive).toBe(false);
+    expect(game.grid[24][25].isAlive).toBe(true);
+    expect(game.grid[24][26].isAlive).toBe(false);
+    expect(game.grid[25][24].isAlive).toBe(false);
+    expect(game.grid[25][25].isAlive).toBe(true);
+    expect(game.grid[25][26].isAlive).toBe(false);
+    expect(game.grid[26][24].isAlive).toBe(false);
+    expect(game.grid[26][25].isAlive).toBe(true);
+    expect(game.grid[26][26].isAlive).toBe(false);
+  });
+
+  test('isAlive correct after 1 step for top and bottom edge cells', () => {
+    expect(game.numRows).toBe(50);
+    expect(game.numCols).toBe(50);
+    expect(game.grid[49][14].isAlive).toBe(false);
+    expect(game.grid[49][15].isAlive).toBe(true);
+    expect(game.grid[49][16].isAlive).toBe(false);
+    expect(game.grid[0][14].isAlive).toBe(false);
+    expect(game.grid[0][15].isAlive).toBe(true);
+    expect(game.grid[0][16].isAlive).toBe(false);
+    expect(game.grid[1][14].isAlive).toBe(false);
+    expect(game.grid[1][15].isAlive).toBe(true);
+    expect(game.grid[1][16].isAlive).toBe(false);
+  });
+
+  test('isAlive correct after 1 step for corner cells', () => {
+    expect(game.numRows).toBe(50);
+    expect(game.numCols).toBe(50);
+    expect(game.grid[49][49].isAlive).toBe(false);
+    expect(game.grid[49][0].isAlive).toBe(true);
+    expect(game.grid[49][1].isAlive).toBe(false);
+    expect(game.grid[0][49].isAlive).toBe(false);
+    expect(game.grid[0][0].isAlive).toBe(true);
+    expect(game.grid[0][1].isAlive).toBe(false);
+    expect(game.grid[1][49].isAlive).toBe(false);
+    expect(game.grid[1][0].isAlive).toBe(true);
+    expect(game.grid[1][1].isAlive).toBe(false);
   });
 });
