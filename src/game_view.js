ea2215914f68bb2859284d1bc357843dea81eb6b
Renata Cummins <renatacummins@gmail.com>
2020-06-08 12:18:52 -0400

    feature: implement play/pause button in control panel

--- src/game_view.js
+++ src/game_view.js
@@ -2,16 +2,36 @@ class GameView {
   constructor(game, ctx) {
     this.game = game;
     this.ctx = ctx;
+    this.paused = false;
+  }
+
+  animate() {
+    if (!this.paused) {
+      this.game.step();
+      this.game.render(this.ctx);
+      setTimeout(this.animate.bind(this), 300);
+    }
+  }
+
+  setupControlPanel() {
+    const playPauseButton = document.getElementById('play-pause');
+    playPauseButton.addEventListener('click', this.togglePlayPause.bind(this));
   }
 
   start() {
-    const animateCallback = () => {
-      this.game.step();
-      this.game.render(this.ctx);
-      setTimeout(animateCallback, 300);
-    };
+    this.setupControlPanel();
+    this.animate();
+  }
 
-    animateCallback();
+  togglePlayPause(e) {
+    e.preventDefault();
+
+    if (this.paused) {
+      this.paused = false;
+      this.animate();
+    } else {
+      this.paused = true;
+    }
   }
 }
 
