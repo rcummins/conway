0794c33e65c46935e0641b23129d38f21ddb4d0d
Renata Cummins <renatacummins@gmail.com>
2020-06-12 19:37:26 -0400

    feature: add more color schemes

--- src/colors.js
+++ src/colors.js
@@ -1,21 +1,29 @@
 const COLORS = {
   alive: [
     '#ed365b', // bright red
+    '#fa8072', // salmon
+    '#ff69b4', // hot pink
     '#ff8c00', // dark orange
     '#ffdf00', // golden yellow
     '#00cc99', // caribbean green
     '#00ced1', // dark turquoise
+    '#00bfff', // deep sky blue
     '#007fff', // azure
-    '#966fd6'  // dark pastel purple
+    '#966fd6', // dark pastel purple
+    '#800080'  // purple
   ],
   dead: [
     '#fac6d1', // light red
+    '#fee6e4', // light salmon
+    '#ffdfef', // light pink
     '#ffdcb1', // light orange
     '#fffad8', // light yellow
     '#ccfff2', // light green
     '#d1feff', // light turquoise
+    '#d8f5ff', // light sky blue
     '#c4e1ff', // light blue
-    '#e4daf4'  // light purple
+    '#e4daf4', // light pastel purple
+    '#ffe2ff'  // light purple
   ]
 };
 
